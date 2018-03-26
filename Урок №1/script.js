d0c977eb1f9f2397bbdcd24986d96d79ee31b986
danlisov <32455650+danlisov@users.noreply.github.com>
2018-03-26 14:20:20 +0300

    Add files via upload

---
+++ Урок №1/script.js
@@ -0,0 +1,17 @@
+{
+	var budGet = prompt('Ваш бюджет?');
+	var nameShop = prompt('Название вашего магазин?');
+	var shopGoods = new Array();
+	shopGoods[0] = prompt('Какой тип товаров будем продавать?');
+	shopGoods[1] = prompt('Какой тип товаров будем продавать?');
+	shopGoods[2] = prompt('Какой тип товаров будем продавать?');
+	employers = ['Артём', 'Иван', 'Денис'];
+	mainList = {
+		budget: budGet,
+		nameshop: nameShop,
+		shopgoods: shopGoods,
+		Employers: employers.
+		open
+	}; 
+	var dayBudGet = alert('Бюджет на 1 день =' + budGet/30);
+}
