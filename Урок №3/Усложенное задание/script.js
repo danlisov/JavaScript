2c6a12c68b5490ec5976103eaa0942b6d6674e03
danlisov <32455650+danlisov@users.noreply.github.com>
2018-03-28 18:07:00 +0300

    Delete script.js

--- Урок №3/Усложенное задание/script.js
+++
@@ -1,8 +0,0 @@
-{
-	var num=33721;
-	num=(num / 10000) + ((num % 10000)/1000) + ((num % 1000)/100) + ((num % 100)/10);
-	num=Math.floor(num);
-	console.log(num);
-	num = Math.pow(num,3);
-	alert(num);
-}
